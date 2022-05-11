65d521025da2ea0851c58d21609b5736b7cb518a
rookie.310 <rookie.310@kakaostyle.com>
2022-05-11 16:26:06 +0900

    commit

--- common/s3/main.tf
+++ common/s3/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
+#  profile = "your profile"
   region = "ap-northeast-2"
 }
 
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "terraform_bucket" {
   bucket = var.bucket_name
 }
