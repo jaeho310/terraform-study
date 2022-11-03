b0e781d7533647ef6b65de01a6696ed402e70db8
jaeho choi <69457888+jaeho310@users.noreply.github.com>
2022-11-03 22:03:15 +0900

    Update main.tf

--- order-service/main.tf
+++ order-service/main.tf
@@ -4,3 +4,9 @@ module "order-service-main" {
   // source 디렉토리에 있는 variable을 채우줍니다.
   bucket_name = "order-service-bucket-135"
 }
+
+// provider는 module을 당겨온 쪽에서 정의합니다.
+provider "aws" {
+#  profile = "your profile"
+  region = "ap-northeast-2"
+}
